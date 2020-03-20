d3fb7754b9025461e4a3bfcc86e8ae3e0271485a
Jackpeng <757858105@qq.com>
2020-03-20 18:05:14 +0800

    5、添加音阶限制、音域限制

--- app/src/main/java/com/jackpeng/oboemusic/ear_interval/NoteEarTrainingActivity.java
+++ app/src/main/java/com/jackpeng/oboemusic/ear_interval/NoteEarTrainingActivity.java
@@ -1,23 +1,67 @@
 package com.jackpeng.oboemusic.ear_interval;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.jackpeng.lib_baselibrary.mvp.BaseActivity;
 import com.jackpeng.lib_baselibrary.mvp.BaseEarActivity;
 import com.jackpeng.oboemusic.R;
 import com.jackpeng.oboemusic.utils.NoteRandomUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.OnClick;
 
-public class NoteEarTrainingActivity extends BaseEarActivity {
+public class NoteEarTrainingActivity extends BaseEarActivity implements AdapterView.OnItemSelectedListener {
     @BindView(R.id.btn_c_note)
     Button mCNote;
+    @BindView(R.id.btn_cd_note)
+    Button mCDNote;
+    @BindView(R.id.btn_d_note)
+    Button mDNote;
+    @BindView(R.id.btn_de_note)
+    Button mDENote;
+    @BindView(R.id.btn_e_note)
+    Button mENote;
+    @BindView(R.id.btn_f_note)
+    Button mFNote;
+    @BindView(R.id.btn_fg_note)
+    Button mFGNote;
+    @BindView(R.id.btn_g_note)
+    Button mGNote;
+    @BindView(R.id.btn_ga_note)
+    Button mGANote;
+    @BindView(R.id.btn_a_note)
+    Button mANote;
+    @BindView(R.id.btn_ab_note)
+    Button mABNote;
+    @BindView(R.id.btn_b_note)
+    Button mBNote;
+
+
+
+    @BindView(R.id.sp_start_note)
+    Spinner mSpStartNote;
+    @BindView(R.id.sp_end_note)
+    Spinner mSpEndNote;
+
+    int startNoteIndex = 0;
+    int endNoteIndex = 0;
+
+    //默认音域：小字一组
+    String startNoteStr = "c1";
+    String endNoteStr = "b1";
 
     public static Intent getThisIntent(Activity activity){
         Intent intent = new Intent();
@@ -32,12 +76,22 @@ public class NoteEarTrainingActivity extends BaseEarActivity {
 
     @Override
     public void initViews(Bundle savedInstanceState) {
+        startNoteIndex = NoteRandomUtil.getIndexByNoteStr(startNoteStr);
+        endNoteIndex = NoteRandomUtil.getIndexByNoteStr(endNoteStr);
+
+        mSpStartNote.setSelection(startNoteIndex);
+        mSpEndNote.setSelection(endNoteIndex);
+
         nextSoundListen();
+
+        mSpStartNote.setOnItemSelectedListener(this);
+        mSpEndNote.setOnItemSelectedListener(this);
+
     }
 
     @OnClick({R.id.btn_c_note, R.id.btn_cd_note, R.id.btn_d_note, R.id.btn_de_note, R.id.btn_e_note,
             R.id.btn_f_note, R.id.btn_fg_note, R.id.btn_g_note, R.id.btn_ga_note, R.id.btn_a_note,
-            R.id.btn_ab_note, R.id.btn_b_note,R.id.btn_standard_note,R.id.btn_question_note})
+            R.id.btn_ab_note, R.id.btn_b_note,R.id.btn_standard_note,R.id.btn_question_note,R.id.tv_select_note})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_standard_note:
@@ -60,20 +114,177 @@ public class NoteEarTrainingActivity extends BaseEarActivity {
             case R.id.btn_b_note:
                 if (checkResult(view)){
                     //对了，播下一题
+                    view.setSelected(true);
                     nextSoundListen();
                     T("答对了，下一题！");
+                    //所有view都enable
+                    enableAllButton();
+                    view.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            view.setSelected(false);
+                        }
+                    },500);
                 } else {
                     //错了，先啥事不干
-                    T("答错了，继续听听哈~");
+                    T("答错了，再听听~");
+                    view.setEnabled(false);
                 }
+                break;
+            case R.id.tv_select_note:
+                showMultiChoiceDialog();
                 break;
         }
     }
 
+    private void enableAllButton() {
+        mCNote.setEnabled(true);
+        mCDNote.setEnabled(true);
+        mDNote.setEnabled(true);
+        mDENote.setEnabled(true);
+        mENote.setEnabled(true);
+        mFNote.setEnabled(true);
+        mFGNote.setEnabled(true);
+        mGNote.setEnabled(true);
+        mGANote.setEnabled(true);
+        mANote.setEnabled(true);
+        mABNote.setEnabled(true);
+        mBNote.setEnabled(true);
+    }
+
+    boolean[] yourChoice = new boolean[NoteRandomUtil.ALL_NOTE.length];
+    private void showMultiChoiceDialog(){
+        AlertDialog.Builder multiChoiceDialog =
+                new AlertDialog.Builder(NoteEarTrainingActivity.this);
+        multiChoiceDialog.setTitle("请选择要听辩的音阶");
+        // 第二个参数是默认选项，此处设置为0
+        multiChoiceDialog.setMultiChoiceItems(NoteRandomUtil.ALL_NOTE, yourChoice, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                yourChoice[which] = isChecked;
+                T(which+" checked"+isChecked);
+            }
+        });
+        multiChoiceDialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //设置完成
+                updateBtnShow();
+            }
+        });
+        multiChoiceDialog.setNegativeButton("取消",null);
+        multiChoiceDialog.setCancelable(true);
+        multiChoiceDialog.show();
+    }
+
     private void nextSoundListen(){
         //初始音并播放
-        currentNoteStr = NoteRandomUtil.getRandomNote();
+        //currentNoteStr = NoteRandomUtil.getRandomNote();//这个方法直接给全85个音阶
+        //currentNoteStr = NoteRandomUtil.getRandomNoteByRange(startNoteIndex,endNoteIndex);//这个方法限定音域,两参数位置可颠倒
+        //这个方法限定音域,两参数位置可颠倒,而且指定音阶
+        currentNoteStr = NoteRandomUtil.getRandomNoteByRangeWithNotes(startNoteIndex,endNoteIndex,selectNoteStr);
         playSoundFromAssetMayWait(currentNoteStr);
+    }
+
+    List<String> selectNoteStr = new ArrayList<>();
+    private void updateBtnShow(){
+        if (yourChoice.length == 12){
+            if (yourChoice[0]){
+                mCNote.setVisibility(View.VISIBLE);
+                selectNoteStr.add(mCNote.getTag().toString());
+            }else {
+                mCNote.setVisibility(View.INVISIBLE);
+                selectNoteStr.remove(mCNote.getTag().toString());
+            }
+
+            if (yourChoice[1]){
+                mCDNote.setVisibility(View.VISIBLE);
+                selectNoteStr.add(mCDNote.getTag().toString());
+            }else {
+                mCDNote.setVisibility(View.INVISIBLE);
+                selectNoteStr.remove(mCDNote.getTag().toString());
+            }
+
+            if (yourChoice[2]){
+                mDNote.setVisibility(View.VISIBLE);
+                selectNoteStr.add(mDNote.getTag().toString());
+            }else {
+                mDNote.setVisibility(View.INVISIBLE);
+                selectNoteStr.remove(mDNote.getTag().toString());
+            }
+
+            if (yourChoice[3]){
+                mDENote.setVisibility(View.VISIBLE);
+                selectNoteStr.add(mDENote.getTag().toString());
+            }else {
+                mDENote.setVisibility(View.INVISIBLE);
+                selectNoteStr.remove(mDENote.getTag().toString());
+            }
+
+            if (yourChoice[4]){
+                mENote.setVisibility(View.VISIBLE);
+                selectNoteStr.add(mENote.getTag().toString());
+            }else {
+                mENote.setVisibility(View.INVISIBLE);
+                selectNoteStr.remove(mENote.getTag().toString());
+            }
+
+            if (yourChoice[5]){
+                mFNote.setVisibility(View.VISIBLE);
+                selectNoteStr.add(mFNote.getTag().toString());
+            }else {
+                mFNote.setVisibility(View.INVISIBLE);
+                selectNoteStr.remove(mFNote.getTag().toString());
+            }
+
+            if (yourChoice[6]){
+                mFGNote.setVisibility(View.VISIBLE);
+                selectNoteStr.add(mFGNote.getTag().toString());
+            }else {
+                mFGNote.setVisibility(View.INVISIBLE);
+                selectNoteStr.remove(mFGNote.getTag().toString());
+            }
+
+            if (yourChoice[7]){
+                mGNote.setVisibility(View.VISIBLE);
+                selectNoteStr.add(mGNote.getTag().toString());
+            }else {
+                mGNote.setVisibility(View.INVISIBLE);
+                selectNoteStr.remove(mGNote.getTag().toString());
+            }
+
+            if (yourChoice[8]){
+                mGANote.setVisibility(View.VISIBLE);
+                selectNoteStr.add(mGANote.getTag().toString());
+            }else {
+                mGANote.setVisibility(View.INVISIBLE);
+                selectNoteStr.remove(mGANote.getTag().toString());
+            }
+
+            if (yourChoice[9]){
+                mANote.setVisibility(View.VISIBLE);
+                selectNoteStr.add(mANote.getTag().toString());
+            }else {
+                mANote.setVisibility(View.INVISIBLE);
+                selectNoteStr.remove(mANote.getTag().toString());
+            }
+
+            if (yourChoice[10]){
+                mABNote.setVisibility(View.VISIBLE);
+                selectNoteStr.add(mABNote.getTag().toString());
+            }else {
+                mABNote.setVisibility(View.INVISIBLE);
+                selectNoteStr.remove(mABNote.getTag().toString());
+            }
+
+            if (yourChoice[11]){
+                mBNote.setVisibility(View.VISIBLE);
+                selectNoteStr.add(mBNote.getTag().toString());
+            }else {
+                mBNote.setVisibility(View.INVISIBLE);
+                selectNoteStr.remove(mBNote.getTag().toString());
+            }
+        }
     }
 
 
@@ -94,5 +305,22 @@ public class NoteEarTrainingActivity extends BaseEarActivity {
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (view.getId()){
+            case R.id.sp_start_note:
+                startNoteIndex = position;
+                break;
+            case R.id.sp_end_note:
+                endNoteIndex = position;
+                break;
+            default:
+                break;
+        }
+    }
 
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
--- app/src/main/java/com/jackpeng/oboemusic/utils/NoteRandomUtil.java
+++ app/src/main/java/com/jackpeng/oboemusic/utils/NoteRandomUtil.java
@@ -1,20 +1,131 @@
 package com.jackpeng.oboemusic.utils;
 
 import android.text.TextUtils;
+import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class NoteRandomUtil {
-    //部分声音还没录进来
+    //十二音阶
+    public static String[] ALL_NOTE = new String[]
+            {"C","C#(Db)","D","D#(Eb)","E","F","F#(Gb)","G","G#(Ab)","A","A#(Bb)","B"};
+
+    //音阶源：部分声音还没录进来
     public static String[] NOTE_ARRAY = new String[]
-            {"xc1","xcd1","xd1","xde1","xe1","xf1","xfg1","xg1","xga1","xa1"/*,"xab1","xb1"*/
-                    ,"c1","cd1","d1","de1","e1","f1",/*"fg1",*/"g1","ga1","a1"/*,"xab1","xb1"*/
+            {"xc1","xcd1","xd1","xde1","xe1","xf1","xfg1","xg1","xga1","xa1","xab1","xb1"/*大字一组*/
+                    /*,"xc","xcd","xd","xde","xe","xf","xfg","xg","xga","xa","xab","xb"*//*大字组*/
+                    /*,"c","cd","d","de","e","f","fg","g","ga","a","ab","b"*//*小字组*/
+                    ,"c1","cd1","d1","de1","e1","f1","fg1","g1","ga1","a1","ab1","b1"/*小字一组*/
+                    ,"c2","cd2","d2","de2","e2","f2","fg2","g2","ga2","a2"/*,"ab2","b2"*//*小字二组*/
+                    /*,"c3","cd3","d3","de3","e3","f3","fg3","g3","ga3","a3","ab3","b3"*//*小字三组*/
+                    /*,"c4","cd4","d4","de4","e4","f4","fg4","g4","ga4","a4","ab4","b4"*//*小字四组*/
+                    /*,"c5"*//*小字五组*/
             };
+
+    public static int getIndexByNoteStr(String noteStr){
+        if (TextUtils.isEmpty(noteStr)){
+            return 0;
+        }
+        for (int i=0;i<NOTE_ARRAY.length;i++){
+            if (NOTE_ARRAY[i].equals(noteStr)){
+                return i;
+            }
+        }
+        return 0;
+    }
 
     public static String getRandomNote(){
         Random random = new Random();
         int result = random.nextInt(NOTE_ARRAY.length-1);
         return NOTE_ARRAY[result];
+    }
+
+    public static String getRandomNoteByRange(int startIndex,int endIndex){
+        Random random = new Random();
+        int realStart = startIndex<=endIndex?startIndex:endIndex;
+        if (startIndex==endIndex){
+            if (startIndex<NOTE_ARRAY.length){
+                return NOTE_ARRAY[startIndex];
+            }else {
+                return NOTE_ARRAY[NOTE_ARRAY.length-1];
+            }
+        } else {
+            int result = random.nextInt(Math.abs(endIndex-startIndex));
+            if (realStart+result<NOTE_ARRAY.length){
+                return NOTE_ARRAY[realStart+result];
+            } else {
+                return NOTE_ARRAY[NOTE_ARRAY.length-1];
+            }
+        }
+    }
+
+    public static List<String> mRangeNoteList = new ArrayList<>();
+    public static String getRandomNoteByRangeWithNotes(int startIndex, int endIndex, List<String> limitNotes){
+        if (limitNotes == null || limitNotes.size()<=0){//传空的，那就全音阶都来
+            String randomStr = getRandomNoteByRange(startIndex,endIndex);
+            return randomStr;
+        }
+        //判断传入的集合跟自己的是不是一样
+        Collections.sort(limitNotes);
+        Collections.sort(mRangeNoteList);
+        Log.d("Utils","startIndex is"+startIndex + "=endIndex is"+endIndex);
+        Log.d("Utils","limitNotes.toString() "+limitNotes.toString());
+        Log.d("Utils","mRangeNoteList.toString() "+mRangeNoteList.toString());
+        if (limitNotes.toString().equals(mRangeNoteList.toString())){
+            //一样
+
+        } else {
+            //先拿到区域内的所有音阶
+            mRangeNoteList.clear();
+            //List<String> rangeNotes = new ArrayList<>();
+            for (int i = startIndex;i<=endIndex;i++){
+                String tempNoteStr = NOTE_ARRAY[i];
+                for (String str : limitNotes){
+                    if (tempNoteStr.equals(str)){
+                        //一样的音阶
+                        mRangeNoteList.add(tempNoteStr);
+                    } else if (!tempNoteStr.startsWith("x")){//小字组
+                        //截取掉数字
+                        String tempStr = tempNoteStr.substring(0,tempNoteStr.length()-1);
+                        Log.d("Utils","tempStr is"+tempStr);
+                        if (tempStr.equals(str)){
+                            mRangeNoteList.add(tempNoteStr);
+                        } else {
+                        }
+                    } else {//大字组
+                        //截取掉x
+                        String xTempStr = tempNoteStr.substring(1,tempNoteStr.length()-1);
+                        Log.d("Utils","xTempStr is"+xTempStr);
+                        if (xTempStr.equals(str)){
+                            //一样了
+                            mRangeNoteList.add(tempNoteStr);
+                        } else {
+                            //不一样，再截取
+                            String noneXStr = xTempStr.substring(0,tempNoteStr.length()-1);
+                            Log.d("Utils","noneXStr is"+noneXStr);
+                            if (noneXStr.equals(str)){
+                                mRangeNoteList.add(tempNoteStr);
+                            } else {
+                                //是直没匹配上
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (mRangeNoteList.size()>0){
+            //从这个集合里随机
+            Random random = new Random();
+            int index = random.nextInt(mRangeNoteList.size());
+            return mRangeNoteList.get(index);
+        } else {
+            //根本没这法练习，返回null,让提示用户
+            return null;
+        }
     }
 
     //不同组的音符最终只有12个音名
